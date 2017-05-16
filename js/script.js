1d68604af212b81731bc5df6c685b16a49d726a9
Genroa <genroa@hotmail.fr>
2017-05-16 14:38:42 +0200

    Fixed mess with last commits

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
-var map = L.map('map').setView([51.5, -0.09], 10);
-var layers = {
-	'xxx': L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		attribution: '',
-		maxZoom: 18,
-		id: 'mapbox.streets',
-	})
-};
 
-layers.xxx.addTo(map);
-=======
+// var map = L.map('map').setView([51.5, -0.09], 10);
+// var layers = {
+// 	'xxx': L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+// 		attribution: '',
+// 		maxZoom: 18,
+// 		id: 'mapbox.streets',
+// 	})
+// };
+
+// layers.xxx.addTo(map);
 
 
 $(function() {
@@ -21,4 +20,3 @@ $(function() {
     }
   );
 });
->>>>>>> origin/master
