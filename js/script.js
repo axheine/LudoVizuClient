fc300264d1c99b8ab6009b62aad3fd2ddac4e740
Genroa <genroa@hotmail.fr>
2017-05-16 14:35:15 +0200

    Finished fixing sideNav + content div position

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 $(function() {
 	 $('.button-collapse').sideNav({
-      menuWidth: 350, // Default is 300
+      menuWidth: "auto", // Default is 300
       edge: 'right', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
