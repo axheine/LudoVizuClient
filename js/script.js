ba027aee550a0b51d5fac01082d96e072784048b
Genroa <genroa@hotmail.fr>
2017-05-16 13:55:16 +0200

    Work on page layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 
 
 $(function() {
-	$('.navmenu').offcanvas();
+	 $('.button-collapse').sideNav({
+      menuWidth: 350, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
 });
