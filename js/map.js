f78690e5c63327309aa722efcecc6213af8f0d00
Axel HEINE <axheine@gmail.com>
2017-05-16 16:46:38 +0200

    add map and work on design

---
+++ js/map.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([51.5, -0.09], 10);
+var layers = {
+ 	'xxx': L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+ 		attribution: '',
+ 		maxZoom: 18,
+ 		id: 'mapbox.streets',
+ 	})
+};
+
+layers.addTo(map);
