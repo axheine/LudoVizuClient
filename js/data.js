e8529c2822cdbdad73a78f123ff46dc431a60d33
Genroa <genroa@hotmail.fr>
2017-05-19 13:50:21 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ js/data.js
@@ -0,0 +1,29 @@
+dates = [
+	{
+		date: 1780,
+		title: "Un titre",
+		url: "",
+		extent: [],
+		content: "",
+		author: "",
+		markers: []
+	},
+	{
+		date: 1785,
+		title: "Un titre 2",
+		url: "",
+		extent: [],
+		content: "",
+		author: "",
+		markers: []
+	},
+	{
+		date: 1788,
+		title: "Un titre 3",
+		url: "",
+		extent: [],
+		content: "",
+		author: "",
+		markers: []
+	}
+];
--- js/script.js
+++ js/script.js
@@ -6,10 +6,29 @@ $(function() {
 		closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
 		draggable: true // Choose whether you can drag to open on touch screens
 	});
+	
 	$('.modal').modal();
 
 
 	$('.modal > .content-min-header').on('click', function() {
 		$('.modal').modal('close');
 	});
+
+
+	loadDatesMenu();
+
+
+
 });
+
+
+
+loadDatesMenu = function() {
+	let menu = $("#slide-out");
+	dates.forEach(function(date) {
+		console.log("Date: "+JSON.stringify(date));
+
+		menu.append("<li><a>"+date.date+" : "+date.title+"</a></li>");
+
+	});
+};
